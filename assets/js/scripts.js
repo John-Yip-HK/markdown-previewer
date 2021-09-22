987c3060d2217830b36df6df483a8bf437cf6800
John-Yip-HK <yip4work@gmail.com>
2021-09-22 16:55:17 +0800

    Markdown parsing successful. Need to fix the problem of becoming 0 height of #editor and #preview after resizing.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,11 +11,15 @@ const setTextareaHeight = () => {
   const pageContainer = document.querySelector(".page-container");
   const topBar = document.querySelector(".page-container > .page");
 
-  document.querySelector("textarea").style.height = `${
-    pageContainer.offsetHeight -
-    pageContainer.clientTop * 2 -
-    topBar.offsetHeight
-  }px`;
+  for (let elem of [
+    document.querySelector("#editor"),
+    document.querySelector("#preview"),
+  ])
+    elem.style.height = `${
+      pageContainer.offsetHeight -
+      pageContainer.clientTop * 2 -
+      topBar.offsetHeight
+    }px`;
 };
 
 const setElementsHeights = () => {
@@ -23,22 +27,63 @@ const setElementsHeights = () => {
   setTextareaHeight();
 };
 
-const setModalBodyContent = (elemId) => {
+const setModalContent = (elemId) => {
+  const modalTitle = document.querySelector(".modal-title");
+  const modalBody = document.querySelector(".modal-body");
+
   if (elemId == "editor-enlarge-btn") {
-    document.querySelector(".modal-body").innerHTML =
-      document.getElementById("md-editor").value;
+    modalTitle.innerHTML = "Markdown";
+    modalBody.innerHTML = document.getElementById("editor").value;
   } else if (elemId == "preview-enlarge-btn") {
-    document.querySelector(".modal-body").innerHTML =
-      document.getElementById("md-preview").value;
+    modalTitle.innerHTML = "Preview";
+    modalBody.innerHTML = document.getElementById("preview").value;
   } else {
-    document.querySelector(".modal-body").innerHTML =
-      "Unknown button is clicked!";
+    modalTitle.innerHTML = "Error";
+    modalBody.innerHTML = "Unknown button is clicked!";
+  }
+};
+
+const setEditorContent = () => {
+  document.querySelector("#editor").value = "";
+};
+
+let isInPreview = false;
+
+const changeNavLinkInnerHTML = () => {
+  isInPreview = !isInPreview;
+
+  const navLink = document.querySelector(".nav-link");
+  const editorContainer = document.querySelector("#md-editor");
+  const previewContainer = document.querySelector("#md-preview");
+
+  if (isInPreview) {
+    navLink.innerHTML = "Edit Markdown";
+    previewContainer.style.display = "block";
+    editorContainer.style.display = "none";
+  } else {
+    navLink.innerHTML = "View Preview";
+    previewContainer.style.display = "none";
+    editorContainer.style.display = "block";
   }
 };
 
 window.onload = () => {
-  document.querySelector("#preview").style.display = "none";
   setElementsHeights();
-};
+  setEditorContent();
 
-window.addEventListener("resize", setElementsHeights);
+  window.addEventListener("resize", setElementsHeights);
+  document.querySelector("#editor").addEventListener("input", (event) => {
+    let dirty = marked(event.currentTarget.value);
+    let clean = DOMPurify.sanitize(dirty, { USE_PROFILES: { html: true } });
+
+    let iframe = document.querySelector("#preview");
+    iframe =
+      iframe.contentWindow ||
+      iframe.contentDocument.document ||
+      iframe.contentDocument;
+
+    iframe.document.open();
+    iframe.document.write(clean);
+    iframe.document.close();
+  });
+};
